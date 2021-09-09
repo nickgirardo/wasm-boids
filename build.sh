cbaacea1e8e3b2a0789723e6f9287cda34eedf60
Nick Girardo <nickgirardo@gmail.com>
2021-09-09 15:32:13 -0400

    Move build output to subdirectory

--- build.sh
+++ build.sh
@@ -3,9 +3,19 @@
 # Using a simple build script instead of a more robust build system
 # because this project is so small and simple.
 
+# Clean previous build products if any exist
+[ -d build ] && rm -r build
+
+mkdir build
+
 # Compile
-wat2wasm flock.wat -o flock.wasm
+wat2wasm flock.wat -o build/flock.wasm
 
 # Optimization pass
 # Using -O4 because my wat was handwritten
-wasm-opt flock.wasm -O4 -o flock.wasm
+wasm-opt build/flock.wasm -O4 -o build/flock.wasm
+
+# Copy over other resources
+cp index.html build/
+cp app.js build/
+cp style.css build/
